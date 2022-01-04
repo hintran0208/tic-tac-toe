f9bb982aa9fe96b3ec7e70c32e69899fa43445ff
Hien Tran <tranthanhhien5@gmail.com>
2022-01-04 16:00:52 +0700

    211-integrate-win-logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 /**
  * Global variables
  */
-import { TURN } from './constants.js';
+import { TURN, CELL_VALUE, GAME_STATUS } from './constants.js';
 
 import {
   getCellElementAtIdx,
   getCellElementList,
   getCurrentTurnElement,
   getGameStatusElement,
+  getReplayButtonElement,
 } from './selectors.js';
 import { checkGameStatus } from './utils.js';
 
@@ -20,6 +21,7 @@ console.log(checkGameStatus(['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O']));
 console.log(checkGameStatus(['', 'O', 'X', 'X', '', 'X', 'O', 'X', 'O']));
 
 let currentTurn = TURN.CROSS;
+let gameStatus = GAME_STATUS.PLAYING;
 let isGameEnded = false;
 let cellValues = new Array(9).fill('');
 
@@ -35,15 +37,62 @@ function toggleTurn() {
   }
 }
 
+function updateGameStatus(newGameStatus) {
+  gameStatus = newGameStatus;
+
+  const gameStatusElement = getGameStatusElement();
+  if (gameStatusElement) gameStatusElement.textContent = newGameStatus;
+}
+
+function showReplayButton() {
+  const replayButton = getReplayButtonElement();
+  if (replayButton) replayButton.classList.add('show');
+}
+
+function highlightWinCells(winPosition) {
+  if (!Array.isArray(winPosition) || winPosition.length !== 3) {
+    throw new Error('Invalid win position');
+  }
+
+  for (const position of winPosition) {
+    const cell = getCellElementAtIdx(position);
+    if (cell) cell.classList.add('win');
+  }
+}
+
 function handleCellClick(cell, index) {
   const isClicked = cell.classList.contains(TURN.CIRCLE) || cell.classList.contains(TURN.CROSS);
-  if (isClicked) return;
+  const isEndGame = gameStatus !== GAME_STATUS.PLAYING;
+  if (isClicked || isEndGame) return;
 
   // set selected cell
   cell.classList.add(currentTurn);
 
+  // update cellValues
+  cellValues[index] = currentTurn === TURN.CIRCLE ? CELL_VALUE.CIRCLE : CELL_VALUE.CROSS;
+
   // toggle turn
   toggleTurn();
+
+  // check game status
+  const game = checkGameStatus(cellValues);
+  switch (game.status) {
+    case GAME_STATUS.ENDED: {
+      updateGameStatus(game.status);
+      showReplayButton();
+      break;
+    }
+    case GAME_STATUS.X_WIN:
+    case GAME_STATUS.O_WIN: {
+      updateGameStatus(game.status);
+      showReplayButton();
+      highlightWinCells(game.winPositions);
+
+      break;
+    }
+    default:
+    // playing
+  }
 
   console.log('click', cell, index);
 }
